8223623743483df56d89667db225d2897d30f894
samuelmsilva2v <samuelmsilva@outlook.com.br>
2024-10-28 18:33:06 -0300

    created repositories

---
+++ src/main/java/com/example/demo/infrastructure/repositories/CategoriaRepository.java
@@ -0,0 +1,32 @@
+package com.example.demo.infrastructure.repositories;
+
+import java.util.List;
+
+import com.example.demo.domain.models.entities.Categoria;
+
+public class CategoriaRepository {
+
+	public void insert(Categoria categoria) {
+		// TODO
+	}
+	
+	public void update(Categoria categoria) {
+		// TODO
+	}
+	
+	public void delete(Integer id) {
+		// TODO
+	}
+	
+	public Categoria findById(Integer id) {
+		// TODO
+		return null;
+	}
+	
+	public List<Categoria> findAll() {
+		// TODO
+		return null;
+	}
+	
+	
+}
---
+++ src/main/java/com/example/demo/infrastructure/repositories/ContatoRepository.java
@@ -0,0 +1,30 @@
+package com.example.demo.infrastructure.repositories;
+
+import java.util.List;
+
+import com.example.demo.domain.models.entities.Contato;
+
+public class ContatoRepository {
+
+	public void insert(Contato contato) {
+		// TODO
+	}
+	
+	public void update(Contato contato) {
+		// TODO
+	}
+	
+	public void delete(Integer id) {
+		// TODO
+	}
+	
+	public Contato findById(Integer id) {
+		// TODO
+		return null;
+	}
+	
+	public List<Contato> findAll() {
+		// TODO
+		return null;
+	}
+}
