859a7070bcbb3fe06155b8cf367ff60b39f62aff
samuelmsilva2v <samuelmsilva@outlook.com.br>
2024-10-30 17:26:23 -0300

    implemented categoria controller

--- src/main/java/com/example/demo/application/controllers/CategoriaController.java
+++ src/main/java/com/example/demo/application/controllers/CategoriaController.java
@@ -1,37 +1,52 @@
 package com.example.demo.application.controllers;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.example.demo.domain.models.dtos.CategoriaDto;
+import com.example.demo.domain.models.entities.Categoria;
+import com.example.demo.domain.services.interfaces.CategoriaDomainService;
+
+import jakarta.validation.Valid;
 
 @RestController
 @RequestMapping("/api/categorias")
 public class CategoriaController {
 
+	@Autowired
+	CategoriaDomainService categoriaDomainService;
+	
 	@PostMapping
-	public String post() {
-		// TODO
-		return null;
+	public String post(@Valid @RequestBody CategoriaDto dto) throws Exception {
+		return categoriaDomainService.inserir(dto);
 	}
 	
-	@PutMapping
-	public String put() {
-		// TODO
-		return null;
+	@PutMapping("{id}")
+	public String put(@PathVariable Integer id, @Valid @RequestBody CategoriaDto dto) throws Exception {
+		return categoriaDomainService.atualizar(id, dto);
 	}
 	
-	@DeleteMapping
-	public String delete() {
-		// TODO
-		return null;
+	@DeleteMapping("{id}")
+	public String delete(@PathVariable Integer id) throws Exception {
+		return categoriaDomainService.excluir(id);
 	}
 	
 	@GetMapping
-	public String getAll() {
-		// TODO
-		return null;
+	public List<Categoria> getAll() throws Exception {
+		return categoriaDomainService.consultar();
+	}
+	
+	@GetMapping("{id}")
+	public Categoria getById(@PathVariable Integer id) throws Exception {
+		return categoriaDomainService.consultarPorId(id);
 	}
 }
